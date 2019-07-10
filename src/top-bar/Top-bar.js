18cab7149313c8da9e6d8db52f8831dd7e9dda46
vinicius <Vinicius Rocha>
2019-07-09 23:57:13 -0300

    Add TopBar visual variations

--- src/top-bar/Top-bar.js
+++ src/top-bar/Top-bar.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Top-bar.css';
 import menuIcon from './menu-icon.svg';
+import closeIcon from './close-icon.svg';
 
-const TopBar = () => (
-    <div className="Top-bar p-sides-10px">
-        <input type="text" placeholder="Toque para adicionar"
-            className="big-input h100pct transparent" autoFocus />
-        <button title="Show navigation bar" className="btn-icon">
-            <img src={menuIcon} alt="Open menu" />
-        </button>
-    </div>
-)
+function TopBar() {
+    const [isActive, setIsActive] = useState(false);
+    const activate = () => setIsActive(true);
+    const deactivate = () => setIsActive(false);
+
+    let placeholderText = isActive ? 'Produto' : 'Toque para adicionar';
+
+    return (
+        <div className={`Top-bar p-sides-10px ${isActive ? 'active' : null}`}>
+            <input type="text" placeholder={placeholderText}
+                className="big-input h100pct transparent" onBlur={deactivate}
+                onTouchEnd={activate} onClick={activate} autoFocus />
+            <button title="Open navigation bar" className="btn-icon">
+                <img src={menuIcon} alt="Open navigation bar" />
+            </button>
+            <button title="Cancel product addition" className="btn-icon">
+                <img src={closeIcon} alt="Cancel product addition" />
+            </button>
+        </div>
+    )
+}
+
+function DropDown() {
+    return (
+        <ul>
+
+        </ul>
+    )
+}
 
 export default TopBar;
