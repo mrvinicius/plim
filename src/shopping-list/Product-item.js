dac89ae51b85b75fb0189f786912b02f94e30b6f
vinicius <Vinicius Rocha>
2019-07-11 13:31:37 -0300

    Align ProductItem children and improve NumberSpinner layout

--- src/shopping-list/Product-item.js
+++ src/shopping-list/Product-item.js
@@ -6,7 +6,7 @@ import NumberSpinner from '../number-spinner';
 export default function ProductItem({ name, quantity = 0 }) {
     return (
         <div className="Product-item">
-            <div className="Product-item__name">{name}</div>
+            <span className="Product-item__name">{name}</span>
             <NumberSpinner aria-label="Quantidade" min={0} quantity={quantity} />
         </div>
     )
