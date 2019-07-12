6d64d2d4308a69d9a1630e617309344263847559
vinicius <Vinicius Rocha>
2019-07-12 17:37:52 -0300

    Fix bug on mobile that allows scroll while fixed dropdown is open

--- src/App.js
+++ src/App.js
@@ -1,31 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { withRouter } from 'react-router-dom';
 
 import './App.css';
 import TopBar from './top-bar/Top-bar';
 import BottomBar from './bottom-bar/Bottom-bar';
 import ProductItem from './shopping-list/Product-item';
 
-function App() {
-	const [state, setState] = useState({ products: [] })
+function App({ history }) {
+	const [products, setProducts] = useState([]),
+		[isTopBarActive, setIsTopBarActive] = useState(false);
+
+	const activate = () => {
+		setIsTopBarActive(true);
+
+		if (history.location.state
+			&& history.location.state.alreadyActivatedBefore) {
+
+			history.goForward();
+
+			return;
+		}
+
+		history.replace('', { alreadyActivatedBefore: true })
+		history.push('');
+	};
+	const inactivate = () => setIsTopBarActive(false);
 
 	useEffect(() => {
-		fetchProducts().then(prods => setState({ products: prods }));
+		fetchProducts().then(prods => setProducts(prods));
 	}, []);
 
-	return (
-		<Router>
-			<div className="App">
-				<TopBar />
-				<div className="p-sides-10px">
-					{state.products.map(product =>
-						<ProductItem key={product.id} {...product} />
-					)}
-				</div>
+	useEffect(() => {
+		if (isTopBarActive) {
+			window.addEventListener('popstate', inactivate);
+		}
 
-				<BottomBar />
+		return () => {
+			if (isTopBarActive) {
+				window.removeEventListener('popstate', inactivate)
+			}
+		};
+	}, [isTopBarActive]);
+
+	const dropdownClass = isTopBarActive ? 'active' : '';
+	return (
+
+		<div className="App">
+			<div className={`App__dropdown p-sides-10px white ${dropdownClass}`}>
+				<ul className="list reset-list">
+					<li className="list__item">ADD</li>
+					<li className="list__item">Autocomplete</li>
+					<li className="list__item">Histórico</li>
+				</ul>
 			</div>
-		</Router>
+
+			<TopBar activate={activate}
+				goBack={() => history.goBack()}
+				isActive={isTopBarActive} />
+
+			<div className="p-sides-10px">
+				{products.map(product =>
+					<ProductItem key={product.id} {...product} />
+				)}
+			</div>
+
+			<BottomBar />
+		</div>
+
 	);
 }
 
@@ -34,4 +75,4 @@ function fetchProducts() {
 		.then(response => response.json())
 }
 
-export default App;
+export default withRouter(App);
--- src/top-bar/Top-bar.js
+++ src/top-bar/Top-bar.js
@@ -1,44 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { withRouter } from 'react-router-dom';
+import React from 'react'
 
 import './Top-bar.css';
 import menuIcon from './menu-icon.svg';
 import closeIcon from './close-icon.svg';
 
-function TopBar({ history }) {
-    const [isActive, setIsActive] = useState(false);
-    const activate = () => {
-        setIsActive(true);
-
-        if (history.location.state
-            && history.location.state.alreadyActivatedBefore) {
-
-            history.goForward();
-
-            return;
-        }
-
-        history.replace('', { alreadyActivatedBefore: true })
-        history.push('');
-    };
-
-    const inactivate = () => setIsActive(false);
+function TopBar({ isActive, activate, goBack }) {
 
     const [placeholderText, className] = isActive
         ? ['Produto', 'active']
         : ['Toque para adicionar', ''];
-
-    useEffect(() => {
-        if (isActive) {
-            window.addEventListener('popstate', inactivate);
-        }
-
-        return () => {
-            if (isActive) {
-                window.removeEventListener('popstate', inactivate)
-            }
-        };
-    }, [isActive]);
 
     return (
         <div className={`Top-bar ${className}`}>
@@ -50,19 +20,12 @@ function TopBar({ history }) {
                     <img src={menuIcon} alt="Open navigation bar" />
                 </button>
                 <button title="Cancel product addition" className="btn-icon"
-                    onClick={() => history.goBack()}>
+                    onClick={goBack}>
                     <img src={closeIcon} alt="Cancel product addition" />
                 </button>
-            </div>
-            <div className="Top-bar__drop-down-wrapper p-sides-10px w100pct white">
-                <ul className="list reset-list">
-                    <li className="list__item">ADD</li>
-                    <li className="list__item">Autocomplete</li>
-                    <li className="list__item">Histórico</li>
-                </ul>
             </div>
         </div>
     )
 }
 
-export default withRouter(TopBar);
+export default TopBar;
