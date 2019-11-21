c4d539333e626e1dee616ad96273fc6808203169
Vinicius Rocha <mvinicius.gbo@gmail.com>
2019-11-21 08:48:49 -0300

    Improve product listing

---
+++ src/shared/number-spinner/Number-spinner.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './Number-spinner.css';
+
+export default function NumberSpinner({ quantity = 0, min = 0, max = 99, onChange, ...inputProps }) {
+
+  const update = newValue => {
+    if (newValue >= min && newValue <= max) {
+      onChange(newValue);
+    }
+  }
+
+  return (
+    <div className="Number-spinner">
+      <input type="number"
+        className="Number-spinner__input right"
+        value={quantity}
+        onChange={e => update(parseInt(e.target.value, 10))}
+        onBlur={e => e.target.value = parseInt(e.target.value, 10)}
+        {...inputProps} />
+      <button aria-label="Aumentar"
+        className="Number-spinner__button"
+        onClick={() => update(quantity + 1)}>
+        <img className="invert-y" src={process.env.PUBLIC_URL + '/chevron-white.svg'} alt="" />
+      </button>
+      <button aria-label="Diminuir"
+        className="Number-spinner__button"
+        onClick={() => update(quantity - 1)}>
+        <img src={process.env.PUBLIC_URL + '/chevron-white.svg'} alt="" />
+      </button>
+    </div>
+  )
+}
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,18 +1,32 @@
 function normalizeText(text) {
-    text = text.toLowerCase();
-    text = text.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a');
-    text = text.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e');
-    text = text.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i');
-    text = text.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o');
-    text = text.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u');
-    text = text.replace(new RegExp('[Ç]', 'gi'), 'c');
-    return text;
+  text = text.toLowerCase();
+  text = text.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a');
+  text = text.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e');
+  text = text.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i');
+  text = text.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o');
+  text = text.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u');
+  text = text.replace(new RegExp('[Ç]', 'gi'), 'c');
+  return text;
 }
 
 function partial(fn, ...presetArgs) {
-    return function partiallyApplied(...laterArgs) {
-        return fn(...presetArgs, ...laterArgs);
-    };
+  return function partiallyApplied(...laterArgs) {
+    return fn(...presetArgs, ...laterArgs);
+  };
 }
 
-export { normalizeText, partial };
+const arrayMoveMutate = (array, from, to) => {
+  array.splice(to < 0 ? array.length + to : to, 0, array.splice(from, 1)[0]);
+};
+
+const arrayMove = (array, from, to) => {
+  array = array.slice();
+  arrayMoveMutate(array, from, to);
+  return array;
+};
+
+export {
+  normalizeText,
+  partial,
+  arrayMove
+};
